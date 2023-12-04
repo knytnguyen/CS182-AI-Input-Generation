438b93e8c2592187035eadecf2a2ddec9f81d63f
Kenny Nguyen <knytnguyen@gmail.com>
2023-12-03 19:41:37 -0800

    Initial libxml and file setup

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <libxml/parser.h>
+#include <libxml/tree.h>
+
+int main() {
+    // Start code here
+    printf("Hello! This is the initial setup.\n");
+
+    return 0;
+}
